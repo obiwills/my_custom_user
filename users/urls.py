df4e55048466c59444e0e53afb98321d299c5f7f
Martins Ezendu <mtk902@yahoo.com>
2018-01-22 18:19:35 -0500

    second commit

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,16 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import CustomUserCreationForm
+from django.views.generic import TemplateView
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+
+class AboutPageView(TemplateView):
+	template_name = 'About.html'
+
+
